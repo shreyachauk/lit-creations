10184115e20164d139fc2e038219ce343fffbc57
preesha01 <preesha.shankar@gmail.com>
2021-03-31 14:29:31 -0500

    Update App.js
    added "Preesha made Change"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Preesha Change Edit <code>src/App.js</code> and save to reload.
+          Preesha made Change Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
